2117449495179074809c78bfca1a84db60a4f6c0
Meloncowboy <86897937+Meloncowboy@users.noreply.github.com>
2023-12-21 15:02:55 +0800

    Add files via upload

---
+++ Lab9/9-5-1-1data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Employee
+Values
+('S0001', '¤@¤ß', 'P0001'),
+('S0002', '¤G¸t', 'P0002')
---
+++ Lab9/9-5-1-2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Sales
+(P_id Char(5),
+P_name NVARCHAR(10) NOT NULL,
+Quan INT,
+PRIMARY KEY(P_id)
+)
